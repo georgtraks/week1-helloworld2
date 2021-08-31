b7c326c83584852049af1c75e3eeb35ffd222668
Opilane TTHK <opilane@tthk.ee>
2021-08-31 10:55:24 +0300

    Add Hello World code

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Steven and georg!");
+
+            Console.Read(Hello);
+        }
+    }
+}
